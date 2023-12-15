b78829805a5601e7adfae7d9de67d60ddd4af3f7
JurrienBrondijk <jurrien.brondijk@rabobank.nl>
2023-12-15 12:16:38 +0100

    fix site github?

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,7 +19,7 @@ import { FormsModule } from '@angular/forms';
     HttpClientModule,
     FormsModule,
   ],
-  providers: [AnimalsService],
+  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy},AnimalsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
