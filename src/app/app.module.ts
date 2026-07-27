6a08cffac0f0336d4645135722db21bd4d3b86f4
JurrienBrondijk <jurrien.brondijk@rabobank.nl>
2026-07-27 13:07:40 +0200

    change location strategy to normalize url structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { LocationStrategy, PathLocationStrategy } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,7 +20,7 @@ import { RouterModule } from '@angular/router';
     HttpClientModule,
     FormsModule,
   ],
-  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy},AnimalsService],
+  providers: [{ provide: LocationStrategy, useClass: PathLocationStrategy }, AnimalsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
