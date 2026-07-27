319b9f13e7896e093cc92fdd36b3096c30c25ece
JurrienBrondijk <jurrien.brondijk@rabobank.nl>
2026-07-27 14:34:46 +0200

    fix for pages host

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
 
+const params = new URLSearchParams(window.location.search);
+const preservedPath = params.get('p');
+
+if (preservedPath) {
+  const targetUrl = `${window.location.origin}${preservedPath}`;
+  window.history.replaceState({}, '', targetUrl);
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
