16b6bec3e00b83b767b30355cda2fc58d60da950
JurrienBrondijk <jurrien.brondijk@rabobank.nl>
2026-07-27 15:59:35 +0200

    improved yak page

--- src/app/yak/yak.component.ts
+++ src/app/yak/yak.component.ts
@@ -1,13 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+interface FundraiserEntry {
+  date: string;
+  celebration: string;
+  amount: number;
+  currency: string;
+  cause: string;
+}
 
 @Component({
   selector: 'app-yak',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HttpClientModule],
   templateUrl: './yak.component.html',
   styleUrls: ['./yak.component.scss']
 })
-export class YakComponent {
+export class YakComponent implements OnInit {
+  fundraisers: FundraiserEntry[] = [];
+  totalRaisedUsd = 0;
+  private readonly gbpToUsdRate = 1.27;
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<FundraiserEntry[]>('assets/YAK/fundraisers.json').subscribe((data) => {
+      this.fundraisers = data;
+      this.totalRaisedUsd = this.fundraisers.reduce((sum, fundraiser) => {
+        const amountInUsd = fundraiser.currency === '£'
+          ? fundraiser.amount * this.gbpToUsdRate
+          : fundraiser.amount;
+
+        return sum + amountInUsd;
+      }, 0);
+    });
+  }
 }
